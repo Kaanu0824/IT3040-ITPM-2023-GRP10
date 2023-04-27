8e1b3b58f502610b8d464e31de289cdf7e5bd11f
Prasanth <50902596+prasanth100298@users.noreply.github.com>
2023-04-27 17:01:24 +0530

    user order page added

--- frontend/src/component/cart/Shipping.jsx
+++ frontend/src/component/cart/Shipping.jsx
@@ -37,6 +37,105 @@ const Shipping = ({ history }) => {
   };
 
 
+  return (
+    <>
+      <MetaData title="Shipping Details" />
+
+      <CheckoutSteps activeStep={0} />
+
+      <div className="shippingContainer">
+        <div className="shippingBox">
+          <h2 className="shippingHeading">Shipping Details</h2>
+
+          <form
+            className="shippingForm"
+            encType="multipart/form-data"
+            onSubmit={shippingSubmit}
+          >
+            <div>
+              <HomeIcon />
+              <input
+                type="text"
+                placeholder="Address"
+                required
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <PhoneIcon />
+              <input
+                type="number"
+                placeholder="Phone Number"
+                required
+                value={phoneNo}
+                onChange={(e) => setPhoneNo(e.target.value)}
+                size="10"
+              />
+            </div>
+
+            <div>
+              <PublicIcon />
+
+              <select
+                required
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              >
+                <option value="">Country</option>
+                {Country &&
+                  Country.getAllCountries().map((item) => (
+                    <option key={item.isoCode} value={item.isoCode}>
+                      {item.name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+
+            {country && (
+              <div>
+                <TransferWithinAStationIcon />
+
+                <select
+                  required
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                >
+                  <option value="">City</option>
+                  {State &&
+                    State.getStatesOfCountry(country).map((item) => (
+                      <option key={item.isoCode} value={item.isoCode}>
+                        {item.name}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            )}
+
+            <input
+              type="submit"
+              value="Continue"
+              className="shippingBtn"
+              disabled={state ? false : true}
+            />
+          </form>
+        </div>
+      </div>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <BottomTab />
+    </>
+  );
 };
 
 export default Shipping;
