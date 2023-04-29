a594e9522a7d6632c4b954653732d776c8da4417
Akash <81360490+Akash-co@users.noreply.github.com>
2023-04-29 13:57:58 +0530

    about us file changes

--- frontend/src/component/about/About.jsx
+++ frontend/src/component/about/About.jsx
@@ -42,12 +42,7 @@ const About = () => {
                         Welcome to CIS
                       </span>
                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate id est laborum.
+                        this developed for assignment purpose
                       </p>
                       <p>
                         lus ferri velit sanctus cu, sed at soleat accusata.
