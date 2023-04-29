29863f65eff29f6fd1bb79a0b67e30205bd81317
Prasanth <50902596+prasanth100298@users.noreply.github.com>
2023-04-29 17:13:10 +0530

    form component added

--- frontend/src/component/cart/Cart.jsx
+++ frontend/src/component/cart/Cart.jsx
@@ -47,6 +47,83 @@ const Cart = ({ history }) => {
   };
 
   
+  return (
+    <>
+      {cartItems.length === 0 ? (
+        <div className="emptyCart">
+          <RemoveShoppingCartIcon />
+          <Typography>No Items In Cart</Typography>
+          <Link to="/products">View Products</Link>
+          <BottomTab />
+        </div>
+      ) : (
+        <>
+          <div className="cartPage">
+            <div className="cartHeader">
+              <p>Product</p>
+              <p>Quantity</p>
+              <p>Subtotal</p>
+            </div>
+
+            {cartItems &&
+              cartItems.map((item) => (
+                <div className="cartContainer" key={item.product}>
+                  <CartItemCard item={item} deleteCartItems={deleteCartItems} />
+                  <div className="cartInput">
+                    <button
+                      onClick={() =>
+                        decreaseQuantity(item.product, item.quantity)
+                      }
+                    >
+                      -
+                    </button>
+                    <input type="number" readOnly value={item.quantity} />
+                    <button
+                      onClick={() =>
+                        increaseQuantity(
+                          item.product,
+                          item.quantity,
+                          item.stock
+                        )
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
+                  <p className="cartSubtotal">{`$${
+                    item.price * item.quantity
+                  }`}</p>
+                </div>
+              ))}
+
+            <div className="cartGrossProfit">
+              <div></div>
+              <div className="cartGrossProfitBox">
+                <p>Price Total</p>
+                <p>$ {totalPrice}</p>
+              </div>
+              <div></div>
+              <div className="checkOutBtn">
+                <button onClick={checkoutHandler}>Check Out</button>
+              </div>
+            </div>
+          </div>
+          <ToastContainer
+            position="bottom-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <BottomTab />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Cart;
