761c31b0f28bc53d7bc8f6ca02fe6539b982975d
Prasanth <50902596+prasanth100298@users.noreply.github.com>
2023-05-05 17:58:20 +0530

    backend order bug fixed

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -10,11 +10,6 @@ const {
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/order/new").post(isAuthenticatedUser, createOrder);
-
-router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
-
-router.route("/orders/me").get(isAuthenticatedUser, getAllOrders);
 
 router
   .route("/admin/orders")
