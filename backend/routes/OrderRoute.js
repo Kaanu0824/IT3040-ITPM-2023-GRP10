9e498a33e037db91d134028fc781d4bcf0d4aa7d
Prasanth <50902596+prasanth100298@users.noreply.github.com>
2023-05-05 18:00:02 +0530

    user order route added

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -11,6 +11,12 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 
+router.route("/order/new").post(isAuthenticatedUser, createOrder);
+
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+
+router.route("/orders/me").get(isAuthenticatedUser, getAllOrders);
+
 router
   .route("/admin/orders")
   .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminAllOrders);
