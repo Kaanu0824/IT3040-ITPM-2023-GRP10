6e3ab4f0e02e7b0a283fa71487458f458b1d8da9
Kaanu0824 <Kaanu737@gmail.com>
2023-05-21 07:54:23 +0530

    User Model class backend ode changes

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -11,18 +11,21 @@ const userSchema = new mongoose.Schema({
     minlength: [3, "Please enter a name atleast 3 characters"],
     maxlength: [15, "Name can not big than 15 characters"],
   },
+
   email: {
     type: String,
     required: [true, "Please enter your email"],
     validate: [validator.isEmail, "Please enter a valid email"],
     unique: true,
   },
+
   password: {
     type: String,
     required: [true, "Please enter your password!"],
     minlength: [8, "Password should be greater than 8 characters"],
     select: false,
   },
+  
   avatar: {
     public_id: {
       type: String,
