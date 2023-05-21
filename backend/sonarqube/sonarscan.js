7847d9c4cc632e5b8f315c0a06afe6a527329dcb
Akash <81360490+Akash-co@users.noreply.github.com>
2023-05-21 13:39:10 +0530

    sonarqube updated

--- backend/sonarqube/sonarscan.js
+++ backend/sonarqube/sonarscan.js
@@ -5,7 +5,7 @@ scanner(
     serverUrl: "http://localhost:9000",
     token: "1b480aaad89dfc46c817586955036c93747da027",
     options: {
-      "sonar.projectName": "ITPM_GRP_10",
+      "sonar.projectName": "ITPM_G-10-node-project",
       "sonar.projectDescription": "Here I can add a description of my project",
       "sonar.projectKey": "ITPM_grp_10",
       "sonar.projectVersion": "0.0.1",
