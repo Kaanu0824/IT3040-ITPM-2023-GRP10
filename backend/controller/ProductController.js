3a1f67fcda3bff8b527f4e71f01a614aee208a6e
Akash <81360490+Akash-co@users.noreply.github.com>
2023-05-21 13:48:58 +0530

    product id changes

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -251,4 +251,5 @@ exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-// 
+// exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+// const product = await Product.findById(req.query.productId);
